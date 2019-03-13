e58c0bde9db1dd34e2151009c7f6680ca315bae4
Arthur Yip <arthuryip723@gmail.com>
2019-03-12 23:04:18 -0400

    create company model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Company.create(name: 'Company1', description: 'Description1')
+Company.create(name: 'Company2', description: 'Description2')
